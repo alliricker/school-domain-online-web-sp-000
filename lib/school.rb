12764ccf6e59b29f69650515cf64a68a61dbce2a
Allison Ricker <alliricker@gmail.com>
2020-10-05 21:24:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,16 @@
 class School
-
-  attr_accessor :name, :roster
+attr_accessor :name, :roster
 
   def initialize(name)
     @name = name
     @roster = {}
   end
+
+  def add_a_student(student, grade)
+    roster << student
+    
 end
+
+
 school = School.new("Bayside High School")
 school.roster
