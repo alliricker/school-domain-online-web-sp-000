163c672a03a64f7bb50b6027b096cf10d553e87b
Allison Ricker <alliricker@gmail.com>
2020-10-05 21:28:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,13 @@ attr_accessor :name, :roster
   end
 
   def add_a_student(student, grade)
-    roster << student
+    def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
   end
 
 end
